83acbb699577e09e1caf7e28e107656e1fb7dd58
Yves Nweke <yveshenry84@gmail.com>
2025-09-10 21:29:02 +0100

    multi language array loop

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+const languages = [
+  "C is fun",
+  "Python is cool",
+  "JavaScript is amazing"
+];
+
+let result = "";
+while (i < languages.length){
+    console.log(languages[i]);
+    i++;
+}
