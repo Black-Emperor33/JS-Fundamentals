eeca10300b326076cf9c386beaa7b52f77411765
Yves Nweke <yveshenry84@gmail.com>
2025-09-10 22:07:42 +0100

    c is fun loop

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -4,11 +4,10 @@ const x = Number(arg);
 if (!Number.isInteger(x)) {
   console.log("Missing number of occurrences");
 } else if (x > 0) {
-  let result = "";
+  let output = "";
   for (let i = 0; i < x; i++) {
     output += "C is fun";
-    if (i !== n - 1) result += "\n";
+    if (i !== n - 1) output += "\n";
   }
-  console.log(result);
+  console.log(output);
 }
-
