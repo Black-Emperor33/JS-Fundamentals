0400c3febceb9393ff2bece267df6e2e8a68158f
Yves Nweke <yveshenry84@gmail.com>
2025-09-10 21:53:53 +0100

    c is fun loop

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,6 +1,6 @@
 const x = Number(process.argv[2]);
 
-if (!Number.isInteger(x)) {
+if (!Number.isInteger(x) || x <= 0) {
   console.log("Missing number of occurrences");
 } else {
   let result = "";
